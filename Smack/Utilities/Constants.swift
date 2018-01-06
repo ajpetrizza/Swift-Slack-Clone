dac785ebd8517d3ef2af490cc427f0322f9c538d
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-06 00:53:10 -0500

    Created our auth requests, header, and body as well as our constants

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://swiftsmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//user defaults
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
