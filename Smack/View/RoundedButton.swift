8230972c1289f1a5e26ca17f2e28ad47e0aa0e16
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-06 01:21:09 -0500

    Added functionality to create account button and fixed corner of buttons

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Anthony Petrizza on 1/6/18.
+//  Copyright © 2018 Anthony Petrizza. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    // called when a designable is created in interface builder
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
