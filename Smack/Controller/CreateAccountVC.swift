81999f70200ed571fe329bc7c25ec7b9f4b3122e
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-06 20:42:25 -0500

    Added collection view and set up for the avatarVC

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,9 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    //variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickAvatarPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
     
     @IBAction func pickBGColorPressed(_ sender: Any) {
@@ -35,13 +39,23 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func createAccountPressed(_ sender: Any) {
         guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {return}
         guard let pass = passTxt.text , passTxt.text != "" else {return}
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("Logged in user!", AuthService.instance.authToken)
+                        
+                        print(AuthService.instance.authToken)
+                        
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                            
+                        })
                     }
                 })
             }
---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Anthony Petrizza on 1/6/18.
+//  Copyright © 2018 Anthony Petrizza. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+        
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
