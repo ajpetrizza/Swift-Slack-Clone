221b5bd5da8bc23f23e31696d1c526a237144a3d
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-17 23:45:25 -0500

    Finished logging in users and handling data persistence for reopening the app

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -22,6 +22,13 @@ class ChatVC: UIViewController {
         // adding tap and swipe to reveal and hide the view
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        //checking if we are logged in
+        if AuthService.instance.isLoggedIn {
+            AuthService.instance.findUserByEmail(completion: { (success) in
+                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+            })
+        }
     }
 
     
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    //outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
+        
     }
 
     
@@ -28,5 +34,32 @@ class LoginVC: UIViewController {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameText.text , usernameText.text != "" else {return}
+        guard let pass = passwordText.text , passwordText.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.stopAnimating()
+                        self.spinner.isHidden = true
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
+    //setting up the view
+    func setupView() {
+        spinner.isHidden = true
+        usernameText.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor:smackPurplePlaceholder])
+        passwordText.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor:smackPurplePlaceholder])
+    }
     
 }
