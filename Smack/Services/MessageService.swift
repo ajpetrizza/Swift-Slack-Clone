5405c53426f8d9be31bd0dc9a4939d9c7d7c1c3f
ajpetrizza <ajpetrizza@fullsail.edu>
2018-01-18 00:11:52 -0500

    reached out to api and grabbed all the channels

---
+++ Smack/Services/MessageService.swift
@@ -0,0 +1,47 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Anthony Petrizza on 1/17/18.
+//  Copyright © 2018 Anthony Petrizza. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    // array of channel objects
+    var channels = [Channel]()
+    
+    //GET all the channels
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                if let json = try! JSON(data: data).array {
+                    // loop through the array of json objects and grab each value we need
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+
+                        // create new channel object with our data and add it to our array
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    completion(true)
+                }
+                
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
